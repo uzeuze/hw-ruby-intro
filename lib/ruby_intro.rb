b4a3d7a7a40d6f82e0dd4856cabbaf7be6d575a7
Egemen Can Uze <egemencanuze@gmail.com>
2015-10-31 15:39:16 +0000

    Added answers for Homework 0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,46 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject 0, :+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort.last(2).inject 0, :+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    return false if arr.empty?
+    arr.combination(2).any? {|x, y| x + y == n }
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+   !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+ !!(s.to_i(2) % 4 == 0 if s =~ /^[0-1]+$/)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize isbn, price
+    raise ArgumentError.new("Invalid ISBN or Price") if isbn.empty? || price <= 0 
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    sprintf("$%.2f", @price)
+  end
+  
 end
